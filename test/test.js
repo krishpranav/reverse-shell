ec9a83efee0988d57ebaca934471134f3ec45495
krishpranav <krisna.pranav@gmail.com>
2021-04-05 17:53:33 +0530

    done changes

---
+++ test/test.js
@@ -0,0 +1,23 @@
+//imports
+import test from 'ava';
+import reverseShell from 'this';
+
+test('reverseShell is a function', t => {
+	t.is(typeof reverseShell, 'function');
+});
+
+test('reverseShell returns shell code with /host:port variables', t => {
+	const req = {
+		url: '/foo:bar'
+	};
+	const returnValue = reverseShell(req);
+	t.true(returnValue.indexOf('("foo",bar)') > -1);
+});
+
+test('reverseShell returns usage if host and port aren\'t set', t => {
+	const req = {
+		url: ''
+	};
+	const returnValue = reverseShell(req);
+	t.true(returnValue.indexOf('# Reverse Shell as a Service') === 0);
+});
